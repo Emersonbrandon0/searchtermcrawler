10482b75f1d9d70a3b875002987048ec0ec28b04
Brandon Emerson <emersonbrandon0@gmail.com>
2017-04-27 18:49:08 -0400

    Changed to only allow unvisited links

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
 var Crawler = require("crawler");
 var hyperlink;
 
+var foundArray=[];
+
 var c = new Crawler({
-    maxConnections : 1,
+    maxConnections : 100,
+    //No wait between requests
     rateLimit: 0,
-    // This will be called for each crawled page
+    
     callback : function (error, result, done) {
         if(error){
             console.log(error);
         } else{
             var $ = result.$;
             var page=result.body;
-            var searchTerm='crawler'
 
+            //The search term that we are looking for
+            var searchTerm='crawler';
+
+            //Creates regular expression to search for matches to the search term on the page
             var re = new RegExp(searchTerm, 'i');
             var res=page.match(re);
 
-
-            //var res=page.match(/searchTerm/i);
+            //Only runs if that page contains the search term
             if (res && res.length >0){
-                console.log("Your search term:"+"'"+searchTerm+"'"+" was found at the following locations");
-                console.log("   "+$('title').text());
-                console.log("   Page Location: "+result.options.uri);
+                //Makes sure that the page is not recorded if it has already been found
+                if(foundArray.indexOf(result.options.uri)!=-1){
+                    console.log('Already found!');
+                };
+
+                if(foundArray.indexOf(result.options.uri)==-1){
+                    //Logs info to the console about the page that the searchTerm was found
+                    console.log("Your search term:"+"'"+searchTerm+"'"+" was found at the following location:");
+                    console.log("   "+$('title').text());
+                    console.log("   Page Location: "+result.options.uri);
+                    foundArray.push(result.options.uri);
+                    console.log(foundArray);
+                }
             }
         }
 
         $('a').each(function(index,a){
             if(a['attribs']['href']){
-
                 hyperlink=a['attribs']['href']
                 var pat = /^https?:\/\//i;
-
+                
                 if(pat.test(hyperlink)){
                     c.queue([hyperlink]);
                 }
             }
         });
 
-        done(function(){
-            console.log('Finished');
-        });
+        done();
     }
 });
 
 
+//The starting URL
 
 c.queue(['https://github.com/bda-research/node-crawler']);
-
